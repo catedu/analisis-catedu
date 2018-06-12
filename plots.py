3df19521edb0120f362f67d6975592395b2af2e7
deleyva <jesuslopezdeleyva@gmail.com>
2018-06-12 18:46:23 +0200

    modifico los plots

--- plots.py
+++ plots.py
@@ -10,6 +10,7 @@ import numpy as np
 with open('books.json', 'r') as infile:
     data_books = json.load(infile)
 
+dfb = pd.DataFrame(data_books)
 df = pd.DataFrame(data_books['list'])
 df['iter'] = [i for i in range(len(df.index))]
 df['updates'] = [count['updates'] for count in df['counts']]
@@ -19,7 +20,8 @@ source = ColumnDataSource(data={
 	'title' : df['title'],
 	'iter' : df['iter'],
 	'updates' : df['updates'],
-	'editors' : df['editors']
+	'editors' : df['editors'],
+	'creation': [date['dates']['created'].split('T')[0] for date in dfb['list']]
 })
 
 mapper = CategoricalColorMapper(
@@ -29,7 +31,8 @@ mapper = CategoricalColorMapper(
 
 hover = HoverTool(tooltips=[
 	("Título", '@title'),
-	("Número de actualizaciones", '@updates')
+	("Número de actualizaciones", '@updates'),
+	("Fecha de creación", '@creation')
 ])
 
 p = figure(x_axis_label='Cursos',
@@ -40,40 +43,18 @@ p.circle('iter', 'updates', size=8, source=source, color={
 	'field': 'editors', 'transform': mapper
 }, legend='editors')
 
-# show(p)
+show(p)
 
 with open('traffic.json', 'r') as infile:
     data_traffic = json.load(infile)
 
 df_traffic = pd.DataFrame(data_traffic)
-df_traffic['title'] = [data_traffic[str(i)]['title'] for i in range(len(data_traffic))]
-df_traffic['visitas_totales'] = []
-df_traffic['visitas_unicas'] = []
-df_traffic['descargas'] = []
+df_traffic = pd.DataFrame.transpose(df_traffic)
+print(df_traffic.info())
 
-# Recupero los datos necesarios
-for i in range(len(data_traffic)):
-    try:
-        df_traffic['visitas_totales'].append(data_traffic[str(i)]['traffic']['total'])
-    except:
-        df_traffic['visitas_totales'].append(0)
+visitas_totales = sum([visita.get('total') if visita.get('total') != None else 0 for visita in df_traffic.traffic.values])
+visitas_unicas = sum([visita.get('unique') if visita.get('unique') != None else 0 for visita in df_traffic.traffic.values])
+descargas = sum([visita.get('downloads') if visita.get('downloads') != None else 0 for visita in df_traffic.traffic.values])
 
-    try:
-        df_traffic['visitas_unicas'].append(data_traffic[str(i)]['traffic']['total'])
-    except:
-        df_traffic['visitas_unicas'].append(0)
-
-    try:
-        df_traffic['descargas'].append(data_traffic[str(i)]['traffic']['total'])
-    except:
-        df_traffic['descargas'].append(0)
-
-# for i in range(len(data_traffic)):
-# 	try:
-# 	    if not data_traffic[str(i)]['platforms']['list']:
-# 	        platforms.append(None)
-# 	    else:
-# 	        platforms.append(data_traffic[str(i)]['platforms']['list'])
-# 	except:
-# 	    platforms.append(None)
+print('{} visitas totales\n{} visitas únicas\n{} descargas'.format(visitas_totales, visitas_unicas, descargas))
 
